69897ca1ce48944520114147042e588e492a5fb1
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2024-02-06 12:11:45 -0800

    Test: Remove deprecated test utils

--- src/hostess.rs
+++ src/hostess.rs
@@ -11,24 +11,9 @@ fn evaluate_reservation_request() {
     info!("wow");
 }
 
-// Test if schedules are being assessed correctly.
-//
-// - Schedule 1
-//    - `{1707165008, 1708374608, 64}`
-//    - `{1708374608, 1710793808, 96}`
-//    - `{1710793808, 1711398608, 32}`
-//    - `{1711398608, 1713213008, 128}`
-// - Schedule 2
-//    - `{1707165008, 1707769808, 50}`
-//    - `{1707769808, 1708979408, 80}`
-//    - `{1708979408, 1709584208, 40}`
-//    - `{1709584208, 1712003408, 100}`
-//    - `{1712003408, 1712608208, 20}`
-//    - `{1712608208, 1714422608, 60}`
+/// Test if schedules are being assessed correctly.
 #[cfg(test)]
 mod tests {
-    use crate::ReservationRequest;
-
     // Reservation request that fit neatly inside of a "schedule fence" that has capacity.
     #[test]
     fn within_fences_with_capacity() {
