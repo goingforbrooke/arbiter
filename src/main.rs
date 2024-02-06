b0345106b0314aae6eaf9ec370cacb6474a50eab
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2024-02-05 20:48:26 -0800

    Add RESTful API

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,20 @@
 use log::{debug, error, info, trace, warn};
 
 // External crates.
+use warp::Filter;
 
 // Project modules
 mod logging;
 use logging::setup_native_logging;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let _ = setup_native_logging();
+
+    // `user@host: wget -qO- localhost:4242/reserve/Eisenhorn` -> Hello, Eisenhorn
+    let hello = warp::path!("reserve" / String).map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 4242)).await;
+
     info!("Done");
 }
