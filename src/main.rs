fc21c807df66bfe4bc35aeb3089dd0b9f997087e
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2024-02-05 22:18:44 -0800

    Revert to simpler format b/c filter testing not needed

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,18 @@
 use log::{debug, error, info, trace, warn};
 
 // External crates.
-use warp::{http::Response, Filter};
+use warp::Filter;
 
 // Project modules
 mod logging;
 use logging::setup_native_logging;
-
-// Extract the first parameter (a name) from the path and prepend a greeting to it.
-fn create_greeting() -> impl Filter<Extract = (String,), Error = warp::Rejection> + Copy {
-    warp::path::param().map(|name: String| format!("Hello, {}!", name))
-}
 
 #[tokio::main]
 async fn main() {
     let _ = setup_native_logging();
 
     // `user@host: wget -qO- localhost:4242/hello/Eisenhorn` -> Hello, Eisenhorn
-    let hello_route = warp::path!("hello")
-        .and(create_greeting())
-        // Add created greeting to HTML response body.
-        .map(|greeting: String| Response::builder().body(greeting));
+    let hello_route = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
 
     warp::serve(hello_route).run(([127, 0, 0, 1], 4242)).await;
 
@@ -30,14 +22,12 @@ async fn main() {
 }
 
 #[tokio::test]
-// Test the Warp filter that constructs the greeting
 async fn test_hello() {
-    let test_filter = create_greeting();
+    let filter = hello_route();
 
-    let filter_response = warp::test::request()
+    let api_response = warp::test::request()
+        .method("GET")
         .path("/Eisenhorn")
-        .filter(&test_filter)
-        .await
-        .unwrap();
-    assert_eq!(filter_response, "Hello, Eisenhorn!");
+        .reply(&filter);
+    assert_eq!(api_response.status(), 200);
 }
